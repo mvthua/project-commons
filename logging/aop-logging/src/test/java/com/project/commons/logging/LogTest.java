a68f00cd79e2c4e3ff93f856493f69f3fd72353e
Michael Hua <mvthua@gmail.com>
2013-08-29 19:47:52 +0800

    Code cleanup.

--- logging/aop-logging/src/test/java/com/project/commons/logging/LogTest.java
+++ logging/aop-logging/src/test/java/com/project/commons/logging/LogTest.java
@@ -17,7 +17,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  */
 @ContextConfiguration(locations = "classpath:ctx-test.xml")
 @RunWith(SpringJUnit4ClassRunner.class)
-public class LoggingTest {
+public class LogTest {
 
     /**
      * @
