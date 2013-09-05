762956ec91574ac6e366163a3e177d36227cebd6
Michael Hua <mvthua@gmail.com>
2013-09-05 12:23:04 +0800

    Refactor package.

--- log/aop-log/src/test/java/com/project/commons/log/aop/LogTest.java
+++ log/aop-log/src/test/java/com/project/commons/log/aop/LogTest.java
@@ -2,13 +2,15 @@
  * Copyright (c) 2013.
  * All rights reserved.
  */
-package com.project.commons.log;
+package com.project.commons.log.aop;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.project.commons.log.Sample;
 
 /**
  * @author mhua
