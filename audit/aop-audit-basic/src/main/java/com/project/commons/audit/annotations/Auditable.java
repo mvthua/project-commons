8a1133375e2ad9577dd3784a3590f67f40916327
Michael Hua <mvthua@gmail.com>
2013-09-05 12:03:45 +0800

    Basic auditing module.

---
+++ audit/aop-audit-basic/src/main/java/com/project/commons/audit/annotations/Auditable.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2013.
+ * All rights reserved.
+ */
+package com.project.commons.audit.annotations;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author mhua
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(value = ElementType.METHOD)
+public @interface Auditable {
+}
